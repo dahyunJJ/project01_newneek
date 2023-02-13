d14e33619f096a30dc79bd4fe3034ab9c2d65485
jeongdh <jeongdh1212@gmail.com>
2023-02-13 17:58:06 +0900

    version1.0

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".sec3_slide", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+});
+
+var swiper = new Swiper(".sec8_slide", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".next2",
+    prevEl: ".prev2",
+  },
+});

1afcca8fd96b1a0b88839f7f8ae6ad0120824103
jeongdh <jeongdh1212@gmail.com>
2023-02-14 18:01:27 +0900

    version1.1

--- js/index.js
+++ js/index.js
@@ -25,3 +25,17 @@ var swiper = new Swiper(".sec8_slide", {
     prevEl: ".prev2",
   },
 });
+
+// 모달 영역
+const $open_M = document.querySelector(".open_M");
+const $modal = document.querySelector(".modal");
+const $close = document.querySelector(".close");
+const $top_nav = document.querySelector(".top_nav");
+const $modal_gnb = document.querySelector(".modal_gnb");
+
+$open_M.addEventListener("click", function () {
+  $modal.classList.toggle("on");
+});
+$close.addEventListener("click", function () {
+  $modal.classList.remove("on");
+});

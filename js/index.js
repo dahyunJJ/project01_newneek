dfd8f101ab2c990735a7252fac3fae8dceae9ada
jeongdh <jeongdh1212@gmail.com>
2023-02-15 16:15:17 +0900

    version1.2

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 var swiper = new Swiper(".sec3_slide", {
-  slidesPerView: 5,
-  spaceBetween: 30,
+  slidesPerView: 2,
+  spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
   },
 
   navigation: {
@@ -13,11 +24,22 @@ var swiper = new Swiper(".sec3_slide", {
 });
 
 var swiper = new Swiper(".sec8_slide", {
-  slidesPerView: 5,
-  spaceBetween: 30,
+  slidesPerView: 2,
+  spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
   },
 
   navigation: {
@@ -25,3 +47,39 @@ var swiper = new Swiper(".sec8_slide", {
     prevEl: ".prev2",
   },
 });
+
+// 모달 영역
+const $open_M = document.querySelector(".open_M");
+const $modal = document.querySelector(".modal");
+const $close = document.querySelector(".close");
+const $top_nav = document.querySelector(".top_nav");
+const $modal_gnb = document.querySelector(".modal_gnb");
+
+$open_M.addEventListener("click", function () {
+  $modal.classList.toggle("on");
+});
+$close.addEventListener("click", function () {
+  $modal.classList.remove("on");
+});
+
+// gotop 버튼
+scrollTop("js-button", 300);
+function scrollTop(elem, duration) {
+  let target = document.getElementById(elem);
+
+  target.addEventListener("click", function () {
+    let currentY = window.pageYOffset;
+    let step = duration / currentY > 1 ? 10 : 100;
+    let timeStep = (duration / currentY) * step;
+    let intervalID = setInterval(scrollUp, timeStep);
+
+    function scrollUp() {
+      currentY = window.pageYOffset;
+      if (currentY === 0) {
+        clearInterval(intervalID);
+      } else {
+        scrollBy(0, -step);
+      }
+    }
+  });
+}

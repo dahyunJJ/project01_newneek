a8188697f970f4842dc9133fc32ae968b4589a11
jeongdh <jeongdh1212@gmail.com>
2023-02-17 17:10:24 +0900

    version2.0

--- js/index.js
+++ js/index.js
@@ -48,38 +48,16 @@ var swiper = new Swiper(".sec8_slide", {
   },
 });
 
-// 모달 영역
+// 모바일 메뉴 영역
 const $open_M = document.querySelector(".open_M");
-const $modal = document.querySelector(".modal");
 const $close = document.querySelector(".close");
 const $top_nav = document.querySelector(".top_nav");
-const $modal_gnb = document.querySelector(".modal_gnb");
+const $M_menu = document.querySelector(".M_menu");
+const $M_gnb = document.querySelector(".M_gnb");
 
 $open_M.addEventListener("click", function () {
-  $modal.classList.toggle("on");
+  $M_menu.classList.toggle("on");
 });
 $close.addEventListener("click", function () {
-  $modal.classList.remove("on");
+  $M_menu.classList.remove("on");
 });
-
-// gotop 버튼
-scrollTop("js-button", 300);
-function scrollTop(elem, duration) {
-  let target = document.getElementById(elem);
-
-  target.addEventListener("click", function () {
-    let currentY = window.pageYOffset;
-    let step = duration / currentY > 1 ? 10 : 100;
-    let timeStep = (duration / currentY) * step;
-    let intervalID = setInterval(scrollUp, timeStep);
-
-    function scrollUp() {
-      currentY = window.pageYOffset;
-      if (currentY === 0) {
-        clearInterval(intervalID);
-      } else {
-        scrollBy(0, -step);
-      }
-    }
-  });
-}

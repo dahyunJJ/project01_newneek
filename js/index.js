22c7ceb0af75c90f85d4d816cc47dac4bb4e866a
jeongdh <jeongdh1212@gmail.com>
2023-04-04 17:12:16 +0900

    sub page 수정중

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
 var swiper = new Swiper(".sec3_slide", {
   slidesPerView: 2,
   spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
 
   breakpoints: {
     768: {
@@ -26,10 +22,6 @@ var swiper = new Swiper(".sec3_slide", {
 var swiper = new Swiper(".sec8_slide", {
   slidesPerView: 2,
   spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
 
   breakpoints: {
     768: {
@@ -51,9 +43,18 @@ var swiper = new Swiper(".sec8_slide", {
 // 모바일 메뉴 영역
 const $open_M = document.querySelector(".open_M");
 const $close = document.querySelector(".close");
-const $top_nav = document.querySelector(".top_nav");
 const $M_menu = document.querySelector(".M_menu");
-const $M_gnb = document.querySelector(".M_gnb");
+
+const GNB = `<nav class="M_gnb">
+<a href="#"><span data-hover="뉴스레터">뉴스레터</span></a>
+<a href="#"><span data-hover="피자스테이션">피자스테이션</span></a>
+<a href="#"><span data-hover="뉴닉퀴즈">뉴닉퀴즈</span></a>
+<a href="#"><span data-hover="뉴문뉴답">뉴문뉴답</span></a>
+<a href="#"><span data-hover="캠페인">캠페인</span></a>
+<a href="#"><span data-hover="뉴닉탄생기">뉴닉탄생기</span></a>
+</nav>`;
+
+$M_menu.innerHTML = GNB;
 
 $open_M.addEventListener("click", function () {
   $M_menu.classList.toggle("on");
